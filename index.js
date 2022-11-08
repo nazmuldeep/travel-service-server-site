a98e91fef4620076b3a0ee364c04480ec089cacd
nazmuldip <nazmuldip21@gmail.com>
2022-11-09 03:11:48 +0600

    mongo db connect

--- index.js
+++ index.js
@@ -14,6 +14,29 @@ const port = process.env.Port || 5000;
 app.use(cors());
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ivnriwf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
+});
+
+async function run() {
+    try {
+        const UserDataCollection = client.db('travel').collection('userData');
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const cursor = UserDataCollection.find(query)
+            const users = await cursor.toArray();
+            res.send(users)
+
+        })
+        c
+
+    } finally {
+
+    }
+}
 
 run().catch(error => console.log(error))
 
