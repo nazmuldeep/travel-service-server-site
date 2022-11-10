2d8996690532d4d79cf0df15e2fc52d239b010ce
nazmuldip <nazmuldip21@gmail.com>
2022-11-10 23:29:51 +0600

    updated review delete

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ async function run() {
             res.send(result)
         })
 
-        app.delete('/reviewsdelete/:id', async (req, res) => {
+        app.delete('/reviews/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
             const result = await reviewDataCollection.deleteOne(query)
